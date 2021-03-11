5c7a2220933e50ab01a8080400db2cbc5b15f8e5
walacos98 <79063366+walacos98@users.noreply.github.com>
2021-03-10 19:17:37 -0600

    Correciones preliminares
    Se agrego el Script para la visualizacion de la pagina

--- js/visor.js
+++ js/visor.js
@@ -12,17 +12,47 @@ function inicio(){
 		case "1":
 			total=10;
 			url="images/tex/tex"
-            $("#titulo").html("textiles");
-			break;
-	
+            $("#titulo").html("Textiles");
+		break;
+
+		case "2":
+			total=7;
+			url="images/cera/cera"
+            $("#titulo").html("Ceramica");
+		break;
+
+		case "3":
+			total=2;
+			url="images/vid/vid"
+            $("#titulo").html("Vidrio");
+		break;
+
+		case "4":
+			total=11;
+			url="images/meta/meta"
+            $("#titulo").html("Metales");
+		break;
+
+		case "5":
+			total=8;
+			url="images/acce/acce"
+            $("#titulo").html("Accesorios");
+		break;
+
+		case "6":
+			total=5;
+			url="images/tec/tec"
+            $("#titulo").html("Tecnología");
+		break;
+
 		default:
-			break;
+		break;
 	}
 
-	for (var i = 0; i < total ; i++) {
-		$('#lightgallery').append('<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-src="'+url+i+'.jpg" data-sub-html="<h4>textil1</h4>'+
-            '<p>Puedes personalizar tus productos.</p>">'+
-            '<a href="#"><img src="'+url+i+'.jpg" alt="IMage" class="img-fluid"></a>'+
+	for (var i = 0; i <= total ; i++) {
+		$('#lightgallery').append('<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-src="'+url+i+'.jpg" data-sub-html="<h4>Puedes personalizar tus productos.</h4>'+
+            '<p>Imagen con fines ilustrativos</p>">'+
+            '<a href="#"><img src="'+url+i+'.jpg" alt="image" class="img-fluid" width="900" height="750"></a>'+
             '</div>')
 	}
 }
