6b1d66fc82e1caed18be7a4780eadf03449f3441
Dcponce <dcponce22@gmail.com>
2021-05-15 19:13:12 -0600

    Modificación en contactos y link de whatsapp

--- js/visor.js
+++ js/visor.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 	inicio();
-	modal2();
-	$(".btn").on("click",function(){$("#mensaje").modal("show")})
+	
+	$(".btn").on("click",function(){
+		modal2();
+	})
 });
 
 function inicio(){
@@ -51,8 +53,6 @@ function inicio(){
 		case "6":
 			img=6;
 			url="images/tec/tec"
-			h4="Consultar existencias y precios por WhatsApp";
-			p="";
             $("#titulo").html("Tecnología");
             $("#title").html("Tecnología");
 
@@ -62,25 +62,22 @@ function inicio(){
 					'</div>')
 			}
 		break;
+
+		default:
+			location.href="error404.html";
+			break;
 	}
 
 	for (var i = 0; i < total ; i++) {
 		$('#lightgallery').append('<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" data-src="'+url+i+'.jpg" data-sub-html="<h4>'+h4+'</h4>'+
-            '<p>'+p+'</p>">'+
+            '<p>'+p+'.Consulta en nuestro Whatsapp</p>">'+
             '<a href="#"><img src="'+url+i+'.jpg" alt="image" class="img-fluid" width="900" height="750"></a>'+
             '</div>')
 	}
-
 	
 }
 
 function modal2(){
-	$("body").append('<div class="modal fade" id="mensaje" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">'+
-	'<div class="modal-dialog modal-lg"><div class="modal-content" style="background-image: url(images/msj.jpg);">'+
-	'<div class="modal-header"><h5 class="modal-title" id="exampleModalLabel"></h5>'+
-	'<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true" style="color:#F4F21E;">&times;</span></button></div>'+
-	'<div class="modal-body"><div class="row">'+
-	'<div class="col-md-6"><b><p>Para proporcionarle más información escribanos a nuestro <span style="color:#1E9DBF">WhatsApp</span></p></b></div>'+
-	'<div class="col-md-6"></div></div></div><div class="modal-footer">'+
-	'<button type="button" class="btn btn-success" data-dismiss="modal">Aceptar</button></div></div></div></div>')
+	location.href="https://api.whatsapp.com/send?phone=50375458366&text=Hola,%20estoy%20buscando%20un%20";
 			}
+
